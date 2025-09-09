14f921fadbd6c7f6d18f8673be5aa7a5116ef24c
Daniel Bednarczyk <d.bednarczyk.99@gmail.com>
2025-09-09 21:37:40 +0200

    Zadanie 27.7

--- client/src/components/features/SeatChooser/SeatChooser.js
+++ client/src/components/features/SeatChooser/SeatChooser.js
@@ -10,6 +10,8 @@ const SeatChooser = ({ chosenDay, chosenSeat, updateSeat }) => {
   const dispatch = useDispatch();
   const seats = useSelector(getSeats);
   const requests = useSelector(getRequests);
+  const [numOfFreeSeats, setNumOfFreeSeats] = useState(0);
+  const numOfSeats = 50;
   
   useEffect(() => {
     dispatch(loadSeatsRequest());
@@ -25,12 +27,16 @@ const SeatChooser = ({ chosenDay, chosenSeat, updateSeat }) => {
       dispatch(loadSeats(seats));
     })
 
-    return () => {
+    return () => { 
       socket.disconnect();
-    };
-  }, [dispatch]);
+    }; 
+  }, [ dispatch ]);
 
-  const isTaken = (seatId) => {
+  useEffect(() => {
+    freeSeatsCounter();
+  }, [ seats, chosenDay ]);
+
+  const isTaken = (seatId) => { 
     return (seats.some(item => (item.seat === seatId && item.day === chosenDay)));
   }
 
@@ -40,16 +46,30 @@ const SeatChooser = ({ chosenDay, chosenSeat, updateSeat }) => {
     else return <Button key={seatId} color="primary" className="seats__seat" outline onClick={(e) => updateSeat(e, seatId)}>{seatId}</Button>;
   }
 
+  const freeSeatsCounter = () => {
+    const takenSeats = seats.filter(seat => seat.day === chosenDay);
+    const freeSeats = numOfSeats - takenSeats.length;
+    console.log('seats: ', seats);
+    console.log('freeSeats: ',freeSeats);
+    console.log('takenSeats: ', takenSeats);
+    console.log('takenSeats.length: ', takenSeats.length);
+
+    setNumOfFreeSeats(freeSeats);
+  }
+
   return (
     <div>
       <h3>Pick a seat</h3>
       <div className="mb-4">
-        <small id="pickHelp" className="form-text text-muted ms-2"><Button color="secondary" /> – seat is already taken</small>
-        <small id="pickHelpTwo" className="form-text text-muted ms-2"><Button outline color="primary" /> – it's empty</small>
+        <small id="pickHelp" className="form-text text-muted ms-2"><Button disabled color="secondary" /> – seat is already taken</small>
+        <small id="pickHelpTwo" className="form-text text-muted ms-2"><Button disabled outline color="primary" /> – it's empty</small>
       </div>
-      { (requests['LOAD_SEATS'] && requests['LOAD_SEATS'].success) && <div className="seats">{[...Array(50)].map((x, i) => prepareSeat(i+1) )}</div>}
+      { (requests['LOAD_SEATS'] && requests['LOAD_SEATS'].success) && <div className="seats">{[...Array(numOfSeats)].map((x, i) => prepareSeat(i+1) )}</div>}
       { (requests['LOAD_SEATS'] && requests['LOAD_SEATS'].pending) && <Progress animated color="primary" value={50} /> }
       { (requests['LOAD_SEATS'] && requests['LOAD_SEATS'].error) && <Alert color="warning">Couldn't load seats...</Alert> }
+      <div>
+        <small id="pickHelp" className="form-text text-muted ms-2"> Free seats: {numOfFreeSeats}/{numOfSeats} </small>
+      </div>
     </div>
   )
 }
