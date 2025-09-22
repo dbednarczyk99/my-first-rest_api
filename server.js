f84393f18929ec9ce586c2fb469f7a2646397a31
Daniel Bednarczyk <d.bednarczyk.99@gmail.com>
2025-09-22 14:26:51 +0200

    Utilize MongoDB Atlas

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.use((req, res) => {
 });
 
 // connects our backend code with the database
-mongoose.connect('mongodb://0.0.0.0:27017/NewWaveDB');
+mongoose.connect('mongodb+srv://dbednarczyk99_admin:S329MyQAL8vaLfDH@cluster0.wwhzdbw.mongodb.net/NewWaveDB?retryWrites=true&w=majority&appName=Cluster0');
 const db = mongoose.connection;
 
 db.once('open', () => {
